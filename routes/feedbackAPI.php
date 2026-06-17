b70f09afdc66c6a929c9c8b2733bfe39b2d927c8
mayada451 <mayadai561@gmail.com>
2026-06-18 02:30:54 +0300

    add feedback routes

---
+++ routes/feedbackAPI.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "../controllers/FeedbackController.php";
+$data = json_decode(file_get_contents("php://input"),true);
+$path=$_SERVER['PATH_INFO']?? '';
+
+if($_SERVER['REQUEST_METHOD']=="GET" && isset($_GET['feedbackID']) && $path=='/Feedback'){
+    GetFeedbackById($_GET['feedbackID']);
+}
+if($_SERVER['REQUEST_METHOD']=="GET" && isset($_GET['countidea']) && $path=='/Feedback'){
+    CountFeedbackByIdea($_GET['countidea']);
+}
+if($_SERVER['REQUEST_METHOD']=="GET" && isset($_GET['countsession']) && $path=='/Feedback'){
+    CountFeedbackBySession($_GET['countsession']);
+}
+if($_SERVER['REQUEST_METHOD']=="GET" && $path =='/Feedback'){
+    GetAllFeedback();
+}
+if($_SERVER['REQUEST_METHOD']=="POST" && $path =='/Feedback'){
+    CreateFeedback($data);
+}
+if($_SERVER['REQUEST_METHOD']=="DELETE" && $path=='/Feedback' && isset($_GET['feedbackID'])){
+    DeleteFeedback($_GET['feedbackID']);
+}
