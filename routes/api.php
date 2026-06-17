b4fdb74ee6d75cd6d75769b9b825e79e6dd596d1
mohandayman173 <mohandayman173@gmail.com>
2026-06-18 01:22:38 +0300

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -1,36 +1,24 @@
+// ana mohand
 <?php
-require_once "../connection.php";
-require_once "../controllers/loginCont.php";
-require_once "../controllers/registerCont.php";
-require_once "../controllers/resetCont.php";
-require_once "../controllers/forgetCont.php";
-require_once "../controllers/investorCont.php";
 
-$data = json_decode(file_get_contents("php://input"),true);
-$path = $_SERVER['PATH_INFO'];
 
-if ($_SERVER['REQUEST_METHOD'] == "POST" && $path == '/register') {
-    register($data);
+require_once __DIR__ . "/../db.php";
+require_once "../controllers/idea.php";
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+if ($method == "POST") {
+    postIdea();
 }
 
-if ($_SERVER['REQUEST_METHOD'] == "POST" && $path == '/login'){
-    login($data);
-}
-if ($_SERVER['REQUEST_METHOD'] == "POST" && $path == '/forgot-password') {
-   forgotPassword($data);
-}
-if ($_SERVER['REQUEST_METHOD'] == "POST" && $path == '/reset-password') {
-   resetPassword($data);
-}
-// Investor Get Idea Routes (muzan)
-if ($_SERVER['REQUEST_METHOD'] == "GET" && $path == '/ideas') {
-    if (isset($_GET['id'])) {
-        getIdeaDetailsController($pdo);
-    } else {
-        getIdeasController($pdo);
-    }
-}
-if ($_SERVER['REQUEST_METHOD'] == "GET" && $path == '/ideas/documentations') {
-    getIdeaDocumentationsController($pdo);
+elseif ($method == "GET" && isset($_GET["id"])) {
+    getIdea();
 }
 
+elseif ($method == "DELETE" && isset($_GET["id"])) {
+    deleteIdeacontroller();
+}
+
+elseif ($method == "PUT") {
+    updateIdeacontroller();
+}
