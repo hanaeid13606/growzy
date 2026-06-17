0129738dac763477fbc46e9ab85dcec4db963eb8
muzan-111 <zeyad.sayyedd@gmail.com>
2026-06-17 20:35:26 +0300

    Add investor routes for ideas and documentations

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
+require_once "../connection.php";
 require_once "../controllers/loginCont.php";
 require_once "../controllers/registerCont.php";
 require_once "../controllers/resetCont.php";
 require_once "../controllers/forgetCont.php";
+require_once "../controllers/investorCont.php";
 
 $data = json_decode(file_get_contents("php://input"),true);
 $path = $_SERVER['PATH_INFO'];
@@ -20,3 +22,15 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && $path == '/forgot-password') {
 if ($_SERVER['REQUEST_METHOD'] == "POST" && $path == '/reset-password') {
    resetPassword($data);
 }
+// Investor Get Idea Routes (muzan)
+if ($_SERVER['REQUEST_METHOD'] == "GET" && $path == '/ideas') {
+    if (isset($_GET['id'])) {
+        getIdeaDetailsController($pdo);
+    } else {
+        getIdeasController($pdo);
+    }
+}
+if ($_SERVER['REQUEST_METHOD'] == "GET" && $path == '/ideas/documentations') {
+    getIdeaDocumentationsController($pdo);
+}
+
