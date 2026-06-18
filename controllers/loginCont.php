cb2b352bc0cc4872b29f941e3741c47f2c711089
hana eid <hanaeid136@gmail.com>
2026-06-19 00:10:09 +0300

    updated

--- controllers/loginCont.php
+++ controllers/loginCont.php
@@ -19,7 +19,7 @@ function login($data) {
         return response(404, ['message' => 'User not found']);
     }
 
-    if ($pass != $user['password']) {
+    if (!password_verify($pass, $user['password'])) {
         return response(401, ['message' => 'invalid credentials']);
     }
 
